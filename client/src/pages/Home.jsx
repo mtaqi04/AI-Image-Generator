941278ec22206b62e9e21f934ad3ab3acc32a5c3
Mohammad Taqi <mohammad.taqi2420@gmail.com>
2025-06-06 10:47:06 -0400

    Modification: Navbar edited

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,22 @@ import styled from 'styled-components'
 const Container = styled.div`
   height: 100%;
   overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  displlay: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  @media only screen and (max-width: 768px) {
+    padding: 6px 10px;
+  }
 `;
 
 export const Home = () => {
-  return (
-    <Container>Home</Container>
+  return ( <Container>
+    <Headline>Explore Popular Posts in the Community!</Headline>
+    
+    </Container>
   )
 }
