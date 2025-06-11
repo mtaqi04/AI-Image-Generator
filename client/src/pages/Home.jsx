c600a5d5fb34af1019cc9ca98a33bedf0f343d31
Mohammad Taqi <mohammad.taqi2420@gmail.com>
2025-06-10 21:22:31 -0400

    Added: a header indicating explore the most recent post has been added to Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Headline = styled.div`
+  font-size: 34px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+
+  @media (max-width: 600px) {
+    font-size: 22px;
+  }
+`;
+
+const Span = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.secondary};
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+
+  @media (max-width: 600px) {
+    font-size: 22px;
+  }
+`;
+
+
 const Container = styled.div`
   height: 100%;
   overflow-y: scroll;
   background: ${({ theme }) => theme.bg};
   padding: 30px 30px;
   padding-bottom: 50px;
-  displlay: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   gap: 20px;
@@ -18,8 +45,10 @@ const Container = styled.div`
 
 export const Home = () => {
   return ( <Container>
-    <Headline>Explore Popular Posts in the Community!</Headline>
-    
+    <Headline>Explore Popular Posts in the Community!
+      <Span>Generated with AI</Span>
+    </Headline>
+
     </Container>
   )
 }
